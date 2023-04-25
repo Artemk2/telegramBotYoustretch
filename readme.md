e30393ce7656d1f039366e4a5508c721bb6b653d
artemSmirnov <ortemka@gmail.com>
2023-04-25 20:27:16 +0300

    Initial message

---
+++ readme.md
@@ -0,0 +1,6 @@
+# Телеграмм бот для получения курса беларусской валюты
+Оригинальная статья https://habr.com/ru/articles/715384/
+
+Для старта бота требуется файл src/main/resources/application.properties с содержимым:
+bot.name=OfficialRateOfBYNBot
+bot.token=5888893679:AAGQWAbS0zCMtnQrTeWrVYz9KHj2nUlyVIw
---
+++ src/main/java/org/youstretch/telegram/telegramBotExhangeRate/TelegramBotExhangeRateApplication.java
@@ -0,0 +1,13 @@
+package org.youstretch.telegram.telegramBotExhangeRate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TelegramBotExhangeRateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TelegramBotExhangeRateApplication.class, args);
+	}
+
+}
