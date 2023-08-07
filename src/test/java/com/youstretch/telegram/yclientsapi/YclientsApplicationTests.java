465cd7721bd47a73c26238ff0429cbdb85797f71
artemSmirnov <ortemka@gmail.com>
2023-08-07 21:51:24 +0300

    Добавлен модульный тест

---
+++ src/test/java/com/youstretch/telegram/yclientsapi/YclientsApplicationTests.java
@@ -0,0 +1,18 @@
+package com.youstretch.telegram.yclientsapi;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.youstretch.telegram.yclientsapi.YclientsApplication;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = YclientsApplication.class)
+class YclientsApplicationTests {
+
+	@Test
+	void contextLoads() {
+		// Пустой тест, чтобы проверить, что контекст приложения загружается без ошибок
+	}
+
+}
