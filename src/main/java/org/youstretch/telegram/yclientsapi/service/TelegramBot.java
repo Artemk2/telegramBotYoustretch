69643bad67bafb08c84e08b4ba2fe10bfcaeb560
artemSmirnov <ortemka@gmail.com>
2023-11-09 23:15:53 +0300

    добавлены фотографии

--- src/main/java/org/youstretch/telegram/yclientsapi/service/TelegramBot.java
+++ src/main/java/org/youstretch/telegram/yclientsapi/service/TelegramBot.java
@@ -4,6 +4,7 @@ import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Component;
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.methods.send.SendPhoto;
 import org.telegram.telegrambots.meta.api.methods.send.SendVideo;
 import org.telegram.telegrambots.meta.api.objects.InputFile;
 import org.telegram.telegrambots.meta.api.objects.Message;
@@ -19,6 +20,8 @@ import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.List;
+
+import static java.sql.DriverManager.println;
 
 @Component
 @AllArgsConstructor
@@ -45,6 +48,7 @@ public class TelegramBot extends TelegramLongPollingBot {
 
     @Override
     public void onUpdateReceived(Update update) {
+        println("Telegram bot started");
         if (!update.hasMessage() || !update.getMessage().hasText()) {
             return;
         }
@@ -54,11 +58,17 @@ public class TelegramBot extends TelegramLongPollingBot {
 
         YclientsService yclientsService = new YclientsService();
         String response = null;
+        String photoPath = null;
         //Обработка сообщения
         switch (messageText) {
             case "/start":
                 //startCommandReceived(chatId, update.getMessage().getChat().getFirstName());
                 String name = update.getMessage().getChat().getFirstName();
+
+                //Photo: Шпагат в белом
+                photoPath = "https://259506.selcdn.ru/sites-static/site615630/85b66920-ef55-44ed-867d-ee38faba44d9/85b66920-ef55-44ed-867d-ee38faba44d9-6701015.jpeg";
+                sendPhoto(chatId, photoPath);
+
                 response = "Привет, " + name + ", я - бот студии You Stretch!\n" +
                         "Хотите записаться на занятие? Я помогу Вам выбрать подходящее время и забронировать место.";
                 break;
@@ -73,9 +83,19 @@ public class TelegramBot extends TelegramLongPollingBot {
                 }
                 break;
             case "Админ\uD83D\uDC67":
+                //Photo: Лена у ёлки
+                //photoPath = "https://259506.selcdn.ru/sites-static/site615630/50e0b538-8b40-4f0c-9959-ba553045d907/50e0b538-8b40-4f0c-9959-ba553045d907-6701249.jpeg";
+                //Photo: Татьяна
+                photoPath = "https://259506.selcdn.ru/sites-static/site615630/9f76440a-d973-4f85-ada8-fdb842c9aa4a/9f76440a-d973-4f85-ada8-fdb842c9aa4a-6701720.jpeg";
+                sendPhoto(chatId, photoPath);
                 response = "Контакт администратора @youStretch";
                 break;
             case "На сайт студии":
+                //Photo: 3 девушки в ГЭС
+                //photoPath = "https://259506.selcdn.ru/sites-static/site615630/e5a75a2e-85f7-4ea3-8edc-4ee8d42c8794/e5a75a2e-85f7-4ea3-8edc-4ee8d42c8794-6701338.jpeg";
+                //Photo: 3 девушки на белом полу
+                photoPath = "https://259506.selcdn.ru/sites-static/site615630/115e00c2-4c30-4665-a531-abeefac3c161/115e00c2-4c30-4665-a531-abeefac3c161-6701445.jpeg";
+                sendPhoto(chatId, photoPath);
                 response = "youstretch.ru";
                 break;
             case "Записаться онлайн":
@@ -83,35 +103,76 @@ public class TelegramBot extends TelegramLongPollingBot {
                 DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
                 String formattedDate = today.format(formatter);
                 System.out.println(formattedDate);
-                response = "https://b911101.yclients.com/company/528085/activity/select?o=act" + formattedDate;
+
+                //Photo: Юля в белом на шпагате с сердечком
+                photoPath = "https://259506.selcdn.ru/sites-static/site615630/7b71039d-0143-411b-8900-6aa1fd8660e2/7b71039d-0143-411b-8900-6aa1fd8660e2-6701173.jpeg";
+                sendPhoto(chatId, photoPath);
+                response = "https://b911101.yclients.com/company/528085/activity/select?o=act" + formattedDate + "\n";
+                response = response + "Переходите по ссылки для записи в yclients";
                 break;
             case "Адрес студии":
+                //Photo: Лена и Вика на кубиках
+                photoPath = "https://259506.selcdn.ru/sites-static/site615630/9bb43647-7d97-42e5-bfbf-d59a626a2bf8/9bb43647-7d97-42e5-bfbf-d59a626a2bf8-6701568.jpeg";
+                sendPhoto(chatId, photoPath);
                 response = "м. Менделеевская\n" +
                         "г. Москва, ул. Новослободская, д. 26, корп. 1\nКабинет 233 (2 подъeзд)\nДомофон 233\nВторой этаж";
                 break;
             case "Пробное занятие":
-                response = "600 рублей";
+                //Photo: Даша и Вика в студии
+                photoPath = "https://259506.selcdn.ru/sites-static/site615630/f7cb1ba8-2693-4c34-be44-bbef36506fd3/f7cb1ba8-2693-4c34-be44-bbef36506fd3-6701229.jpeg";
+                sendPhoto(chatId, photoPath);
+                response = "Стоимость 500 рублей";
                 break;
             case "/test":
                 response = "CompanyId = " + getYclientsCompanyId();
                 break;
             case "Купить абонемент":
+                //Photo: 3 девушки на белом полу
+                //photoPath = "https://259506.selcdn.ru/sites-static/site615630/115e00c2-4c30-4665-a531-abeefac3c161/115e00c2-4c30-4665-a531-abeefac3c161-6701445.jpeg";
+                //sendPhoto(chatId, photoPath);
                 response = "https://ailfo.tb.ru/price";
                 break;
             case "Направления":
-                response = "1️⃣Гибкость (60 минут)\n2️⃣Здоровая спина (60 минут)\n\uD83D\uDCAA Рельеф + гибкость (90 минут)\n" +
-                        "Два шпагата (90 минут)\n\uD83E\uDDD8\u200D♀ Гибкость и ️медитация (90 минут)\nРельеф (60 минут)\n" +
-                        "Свободный формат (60-90 минут)\n" +
-                        "Акробатика (60 минут)\nТанцы\nМедитация поющими чашами";
+                //Photo: Вика, руки в форме сердечка
+                photoPath = "https://259506.selcdn.ru/sites-static/site615630/976cf0ee-a1dc-4b4f-934b-9f0384d1228e/976cf0ee-a1dc-4b4f-934b-9f0384d1228e-6701444.jpeg";
+                sendPhoto(chatId, photoPath);
+                response = "✅Гибкость (60 минут)\n" +
+                        "✅Здоровая спина (60 минут)\n" +
+                        "\uD83D\uDCAA Рельеф + гибкость (90 минут)\n" +
+                        "✅Два шпагата (90 минут)\n" +
+                        "\uD83E\uDDD8\u200D♀ Гибкость и ️медитация (90 минут)\n" +
+                        "✅Рельеф (60 минут)\n" +
+                        "✅Свободный формат (60-90 минут)\n" +
+                        "✅Акробатика (60 минут)\n" +
+                        "✅Танцы\n" +
+                        "✅Медитация поющими чашами\n" +
+                        "✅Энергомассаж";
                 break;
             case "Форматы":
-                response = "Форматы занятий:\nИндивидуальные тренировки\nМини группы - 3 человека";
+                //Photo: Лена на полу
+                photoPath = "https://259506.selcdn.ru/sites-static/site615630/684d1f28-006d-4cf6-99b9-cfbe321b0958/684d1f28-006d-4cf6-99b9-cfbe321b0958-6701536.jpeg";
+                sendPhoto(chatId, photoPath);
+                response = "☘️Форматы занятий:\n☘️Индивидуальные тренировки\n☘️Мини группы - 3 человека";
                 break;
             case "Найти студию":
                 response = "sendVideo";
+                //String videoPath = "https://youtu.be/E85_wBc57zY";
+                String videoPath = "https://youtu.be/CzY5eKWwVEo";
+                sendVideo(chatId, videoPath);
+                break;
+            case "sendPhoto":
+                //Юля на сапе
+                photoPath = "https://259506.selcdn.ru/sites-static/site615630/035f36b7-1e6d-4b30-ad9f-3c337671aa36/035f36b7-1e6d-4b30-ad9f-3c337671aa36-5665126.jpeg";
+                //response = "sendPhoto";
+                sendPhoto(chatId, photoPath);
                 break;
             case "Чат\uD83D\uDCAC":
-                response = "https://t.me/+aoFPJzY-bP6LyVR3";
+                //Photo: Несколько людей на фоне воды
+                photoPath = "https://259506.selcdn.ru/sites-static/site615630/2bd60e00-1c72-48a4-af3b-2a99ab19831d/2bd60e00-1c72-48a4-af3b-2a99ab19831d-6701008.jpeg";
+                sendPhoto(chatId, photoPath);
+                response = "https://t.me/+aoFPJzY-bP6LyVR3 \n";
+                response = response + "В чате общаются клиенты студии и получают информацию о мероприятиях. ";
+                response = response + "Пожалуйста, делитесь фотографиями. Присоединяйтесь и вы!";
                 break;
             case "Получить тренеров":
                 try {
@@ -133,38 +194,60 @@ public class TelegramBot extends TelegramLongPollingBot {
         if (response == null) {
             response = "ошибка в работе бота.\nОтветное сообщение не подготовлено";
         }
-        if (!response.equals("sendVideo")) {
-            sendMessage(chatId, response);
-        } else {
-            // Путь к видеофайлу
-            String videoFilePath = "src/main/resources/videoRoute.mp4";
-            //Отправка видео
-            try {
-                // Создаем объект для отправки видео
-                SendVideo sendVideo = new SendVideo();
-                sendVideo.setChatId(chatId);
-                sendVideo.setVideo(new InputFile(videoFilePath));
 
-                // Отправляем видео
-                execute(sendVideo);
+        sendMessage(chatId, response);
+        //end
+    }
+
+    private void sendVideo(long chatId, String videoPath) {
+        //Отправка видео
+        try {
+            // Создаем объект для отправки видео
+            SendVideo sendVideo = new SendVideo();
+            sendVideo.setChatId(chatId);
+            sendVideo.setVideo(new InputFile(videoPath));
+
+            // Отправляем видео
+            execute(sendVideo);
+        } catch (TelegramApiException e) {
+            e.printStackTrace();
+            sendErrorMessageTelegramApiException(chatId, e);
+        }
+    }
+
+
+    private void sendPhoto(long chatId, String photoPath) {
+        SendPhoto sendPhoto = new SendPhoto();
+        sendPhoto.setChatId(chatId);
+        if (photoPath.isBlank()) {
+            // error message
+            sendMessage(chatId, "Путь к файлу не задан");
+        } else {
+            // Указываем путь к фотографии (локальный или URL)
+            sendPhoto.setPhoto(new InputFile(photoPath));
+
+            try {
+                execute(sendPhoto);
             } catch (TelegramApiException e) {
                 e.printStackTrace();
-                sendErrorMessageTelegramApiException(chatId, e);
+                // Обработка ошибки отправки фотографии
             }
         }
-
     }
 
-
+    //sendErrorMessage
     private void sendErrorMessageIOException(long chatId, IOException e) {
         String error = String.valueOf(e);
-        errorMessage(chatId,error);
-    }private void sendErrorMessageTelegramApiException(long chatId, TelegramApiException e) {
-        String error = String.valueOf(e);
-        errorMessage(chatId,error);
+        errorMessage(chatId, error);
     }
-    private void errorMessage(long chatId, String error){
-        String message = "Произошла ошибка при работе telegram бота yclients:\n" ;
+
+    private void sendErrorMessageTelegramApiException(long chatId, TelegramApiException e) {
+        String error = String.valueOf(e);
+        errorMessage(chatId, error);
+    }
+
+    private void errorMessage(long chatId, String error) {
+        String message = "Произошла ошибка при работе telegram бота yclients:\n";
         sendMessage(chatId, message);
         sendMessage(chatId, error);
     }
